920c327af74bfe052165c7c741ad866e780dec36
gulkayir1 <de.gulkayir@gmail.com>
2021-11-22 12:33:53 +0600

    registration, authorization, tokens, login, logout

---
+++ main/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from main.models import Category, Post, PostImage
+
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    max_num = 10
+    min_num = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline, ]
+
+admin.site.register(Category)
---
+++ main/urls.py
@@ -0,0 +1,11 @@
+# from django.urls import path
+#
+# from main import views
+#
+# urlpatterns = [
+#     path('categories/', views.CategoryListView.as_view(), name='categories-list'),
+#     path('posts/', views.PostView.as_view(), name='posts-list'),
+#     path('posts/<int:pk>/', views.PosDetailView.as_view(), name='post-detail'),
+#     path('posts-update/<int:pk>/', views.PosUpdateView.as_view()),
+#     path('posts-delete/<int:pk>/', views.PosDeleteView.as_view())
+# ]

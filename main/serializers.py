9fdaa8e32899c568a54874e02861b7a6999fd690
gulkayir1 <de.gulkayir@gmail.com>
2021-11-23 11:21:24 +0600

    permissions, pagination, filtering

--- main/serializers.py
+++ main/serializers.py
@@ -12,12 +12,22 @@ class PostSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format='%d/%m/%Y %H%M%S', read_only=True)
     class Meta:
         model = Post
-        fields = ('id', 'title', 'category', 'author', 'created_at', 'text')
+        fields = ('id', 'title', 'category', 'created_at', 'text')
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        representation['author'] = instance.author.email
+        representation['category'] = CategorySerializer(instance.category).data
         representation['images'] = PostImageSerializer(instance.images.all(), many=True, context=self.context).data
         return representation
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user_id = request.user.id
+        validated_data['author_id'] = user_id
+        post = Post.objects.create(**validated_data)
+        return post
+
 
 class PostImageSerializer(serializers.ModelSerializer):
    class Meta:
--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
+from datetime import timedelta
+
 from django.db.models import Q
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework.decorators import api_view, action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics, viewsets, status
 
 from .models import Category, Post, PostImage
 from .serializers import CategorySerializer, PostSerializer, PostImageSerializer
-
+from .permissions import IsPostAuthor
 
 #
 # @api_view(['GET'])
@@ -51,14 +56,56 @@ from .serializers import CategorySerializer, PostSerializer, PostImageSerializer
 #     serializer_class = PostSerializer
 #
 
+class MyPaginationClass(PageNumberPagination):
+    page_size = 3
+
+    def get_paginated_response(self, data):
+        for i in range(self.page_size):
+            text = data[i]['text']
+            data[i]['text'] = text[:15] + '...'
+        return super().get_paginated_response(data)
+
+
+
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [AllowAny, ]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, ]
+    pagination_class = MyPaginationClass
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+    def get_permissions(self):
+        """ переопределение метода"""
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permissions = [IsPostAuthor, ]
+        else:
+            permissions = [IsAuthenticated, ]
+        return [permission() for permission in permissions]
+
+    @action(detail=False, methods=['get'])
+    def own(self, request, pk=None):
+        queryset = self.get_queryset()
+        queryset = queryset.filter(author=request.user)
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        weeks_count = int(self.request.query_params.get('week', 0))
+        if weeks_count > 0:
+            start_date = timezone.now() - timedelta(weeks=weeks_count)
+            queryset = queryset.filter(created_at__gte=start_date)
+        return queryset
 
     @action(detail=False, methods=['get'])
     def search(self, request, pk=None):
